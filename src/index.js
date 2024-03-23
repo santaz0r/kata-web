cf86bf5b546f063b4da7bbdcbe29048838c69d7b
VictorM <neuron96@mail.ru>
2024-03-23 21:37:16 +0500

    feat: done final task

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import footer from "./components/Footer";
-import header from "./components/Header";
-import InfoSection from "./components/InfoSection";
-import Menu from "./components/Menu";
-import Modal from "./components/Modal";
-import ServicesSection from "./components/ServicesSection";
 import "./index.scss";
-
-import InitSwipers from "./js/initSwipers";
+import RenderMainPage from "./js/RenderMainPage";
 
 const main = document.querySelector(".main");
 const page = document.querySelector(".page");
 const pageWrapper = document.querySelector(".page__wrapper");
 
-const infoSection = InfoSection();
-const servicesSection = ServicesSection();
-const modal = Modal({ title: "Lorem" });
-const menu = Menu();
-
-page.prepend(modal);
-main.prepend(header);
-pageWrapper.prepend(menu);
-main.append(infoSection);
-main.append(servicesSection);
-main.append(footer);
-
-InitSwipers();
+RenderMainPage({
+  main,
+  page,
+  pageWrapper,
+});
